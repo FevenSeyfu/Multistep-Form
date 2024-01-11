8122afd8613981b1f37016b14987373ae79e5f08
FevenSeyfu <fevenseyfu00@gmail.com>
2024-01-11 17:58:48 +0300

    add active style to status bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,37 @@ import React, { useState } from "react";
 import BgImage from "./assets/images/bg-sidebar-desktop.svg";
 
 const App = () => {
-  const [progresStep, setProgresStep] = useState();
+  const steps = [
+    {
+      id: 1,
+      step: "STEP 1",
+      title: "YOUR INFO",
+    },
+    {
+      id: 2,
+      step: "STEP 2",
+      title: "SELECT PLAN",
+    },
+    {
+      id: 3,
+      step: "STEP 3",
+      title: "ADD-ONS",
+    },
+    {
+      id: 4,
+      step: "STEP 4",
+      title: "SUMMARY",
+    },
+  ];
+  const [currentStep, setCurrentStep] = useState(0);
+  const nextStep = () => {
+    setCurrentStep((prevStep) => (prevStep + 1) % steps.length);
+  };
+
+  const prevStep = () => {
+    setCurrentStep((prevStep) => (prevStep - 1 + steps.length) % steps.length);
+  };
+
   return (
     <div className="bg-white w-[70vw] h-[80vh] rounded-xl p-3 flex gap-24">
       <div
@@ -11,50 +41,20 @@ const App = () => {
       >
         <table class="table-fixed">
           <tbody className="flex flex-col text-white mt-8 mx-8 gap-4 ">
-            <tr className="flex flex-row gap-4  items-center">
-              <td>
-                <div className="border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1">
-                  1
-                </div>
-              </td>
-              <td>
-                <p className="text-neutral-light-gray text-sm">STEP 1</p>
-                <span className="text-sm font-medium">YOUR INFO</span>
-              </td>
-            </tr>
-            <tr className="flex flex-row gap-4 items-center">
-              <td>
-                <div className="border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1">
-                  2
-                </div>
-              </td>
-              <td>
-                <p className="text-neutral-light-gray text-sm">STEP 2</p>
-                <span className="text-sm font-medium">SELECT PLAN</span>
-              </td>
-            </tr>
-            <tr className="flex flex-row gap-4 items-center">
-              <td>
-                <div className="border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1">
-                  3
-                </div>
-              </td>
-              <td>
-                <p className="text-neutral-light-gray text-sm">STEP 3</p>
-                <span className="text-sm font-medium">ADD-ONS</span>
-              </td>
-            </tr>
-            <tr className="flex flex-row gap-4 items-center">
-              <td>
-                <div className="border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1">
-                  4
-                </div>
-              </td>
-              <td>
-                <p className="text-neutral-light-gray text-sm">STEP 4</p>
-                <span className="text-sm font-medium">SUMMARY</span>
-              </td>
-            </tr>
+            {steps.map((step, index) => (
+              <tr className="flex flex-row gap-4  items-center" key={index}>
+                <td>
+                  <div className={`border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1 ${
+              index === currentStep && ("bg-primary-pastel-blue text-black text-bold") }`}>
+                    {step.id}
+                  </div>
+                </td>
+                <td>
+                  <p className="text-neutral-light-gray  text-sm">{step.step}</p>
+                  <span className="font-medium">{step.title}</span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
