fea7a454691d4f282af0c851c275f16a1e8da5f4
FevenSeyfu <fevenseyfu00@gmail.com>
2024-02-02 14:08:13 +0300

    Complete step 1 form and connect with context action

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,51 @@
-import React, { useState } from "react";
-import BgImage from "./assets/images/bg-sidebar-desktop.svg";
-import FormStep1 from "./components/FormStep1";
-import FormStep2 from "./components/FormStep2";
-import FormStep3 from "./components/FormStep3";
-import FormStep4 from "./components/FormStep4";
+import React from 'react';
+import { useFormContext } from './context/FormContext';
+import BgImage from './assets/images/bg-sidebar-desktop.svg';
+import FormStep1 from './components/FormStep1';
+import FormStep2 from './components/FormStep2';
+import FormStep3 from './components/FormStep3';
+import FormStep4 from './components/FormStep4';
 
 const App = () => {
   const steps = [
     {
       id: 1,
-      step: "STEP 1",
-      title: "YOUR INFO",
+      step: 'STEP 1',
+      title: 'YOUR INFO',
     },
     {
       id: 2,
-      step: "STEP 2",
-      title: "SELECT PLAN",
+      step: 'STEP 2',
+      title: 'SELECT PLAN',
     },
     {
       id: 3,
-      step: "STEP 3",
-      title: "ADD-ONS",
+      step: 'STEP 3',
+      title: 'ADD-ONS',
     },
     {
       id: 4,
-      step: "STEP 4",
-      title: "SUMMARY",
+      step: 'STEP 4',
+      title: 'SUMMARY',
     },
   ];
-  const [currentStep, setCurrentStep] = useState(0);
-  const nextStep = () => {
-    setCurrentStep((prevStep) => (prevStep + 1) % steps.length);
-  };
-
-  const prevStep = () => {
-    setCurrentStep((prevStep) => (prevStep - 1 + steps.length) % steps.length);
-  };
-
+  const { state } = useFormContext();
   return (
     <div className="bg-white w-[70vw] h-[80vh] rounded-xl p-3 flex gap-24">
       <div
         className="w-[270px] rounded-xl h-full bg-cover bg-repeat bg-bottom"
         style={{ backgroundImage: `url(${BgImage})` }}
       >
-        <table class="table-fixed">
+        <table className="table-fixed">
           <tbody className="flex flex-col text-white mt-8 mx-8 gap-4 ">
             {steps.map((step, index) => (
               <tr className="flex flex-row gap-4  items-center" key={index}>
                 <td>
-                  <div className={`border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1 ${
-              index === currentStep && ("bg-primary-pastel-blue text-black text-bold") }`}>
+                  <div
+                    className={`border border-neutral-light-gray text-center rounded-full w-[30px] h-[30px] text-sm pt-1 ${
+                      step.id === state.step && 'bg-primary-pastel-blue text-black text-bold'
+                    }`}
+                  >
                     {step.id}
                   </div>
                 </td>
@@ -63,15 +59,15 @@ const App = () => {
         </table>
       </div>
       <div className="">
-      {(() => {
-          switch (currentStep) {
-            case 0:
-              return <FormStep1 />;
+        {(() => {
+          switch (state.step) {
             case 1:
-              return <FormStep2 />;
+              return <FormStep1 />;
             case 2:
-              return <FormStep3 />;
+              return <FormStep2 />;
             case 3:
+              return <FormStep3 />;
+            case 4:
               return <FormStep4 />;
             default:
               return null;
--- src/components/FormStep1.jsx
+++ src/components/FormStep1.jsx
@@ -1,12 +1,23 @@
 import React from "react";
+import { useFormContext } from "../context/FormContext";
 
 const FormStep1 = () => {
+  const { state, dispatch } = useFormContext();
+
+  const handleChange = (fieldName, e) => {
+    dispatch({
+      type: "UPDATE_FORM_DATA",
+      payload: { [fieldName]: e.target.value },
+    });
+  };
+  
+
   return (
     <div className="flex flex-col my-8 gap-4">
       <div>
         <h1 className="font-extrabold text-3xl">Personal info</h1>
         <p className="text-sm text-neutral-cool-gray leading-none">
-          Please provide your name,email and phone number.
+          Please provide your name, email, and phone number.
         </p>
       </div>
       <form className="flex flex-col gap-2 ">
@@ -19,31 +30,40 @@ const FormStep1 = () => {
           id="name"
           placeholder="e.g Stephen King"
           className="w-full border border-neutral-cool-gray p-2 rounded-md mb-2"
+          value={state.formData.name}
+          onChange={(e) => handleChange('name', e)}
         />
         <label htmlFor="email" className="font-bold">
           Email Address
         </label>
         <input
-          type="text"
+          type="email"
           name="email"
           id="email"
           placeholder="e.g stephenking@lorem.com"
           className="w-full border border-neutral-cool-gray p-2 rounded-md mb-2"
+          value={state.formData.email}
+          onChange={(e) => handleChange('email', e)}
         />
         <label htmlFor="phone" className="font-bold">
           Phone Number
         </label>
         <input
-          type="text"
+          type="tel"
           name="phone"
           id="phone"
           placeholder="e.g.+1 234 456 890"
           className="w-full border border-neutral-cool-gray p-2 rounded-md mb-4"
+          value={state.formData.phone}
+          onChange={(e) => handleChange('phone', e)}
         />
         <div className="flex justify-end mt-8">
-        <button className="bg-primary-marine-blue text-white rounded-md w-32 p-3 ">
-          Next Step
-        </button>
+          <button
+            className="bg-primary-marine-blue text-white rounded-md w-32 p-3"
+            onClick={() => dispatch({ type: "NEXT_STEP" })}
+          >
+            Next Step
+          </button>
         </div>
       </form>
     </div>
